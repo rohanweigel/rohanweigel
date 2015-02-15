0395934b2504664d1e7bfd6b309f25fa46efbdae
rohanweigel <rohanweigel@users.noreply.github.com>
2015-02-15 15:01:06 -0500

    HW2_rweigel

---
+++ gradstats.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function () {
+    
+    $.getJSON("https://hivelab.org/static/students.json", function (grads_elmts) {
+        
+        for (var i=1; i < grads_elmts.length; i++) {
+            var rowid;
+            rowid = $('<tr/>');
+            rowid.append("<td>" + grads_elmts[i].Name + "</td>");
+            rowid.append("<td>" + grads_elmts[i].GPA + "</td>");
+            rowid.append("<td>" + grads_elmts[i].GRE_V + "</td>");
+            rowid.append("<td>" + grads_elmts[i].GRE_Q + "</td>");
+            rowid.append("<td>" + grads_elmts[i].Essay + "</td>");
+            rowid.append("<td>" + grads_elmts[i].Recom + "</td>");
+            $('#grad_data').append(rowid);
+        });
+    });
+});
